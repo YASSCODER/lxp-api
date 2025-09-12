3afbd8548e8f399f47de36edb532423fb89503f6
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2025-09-12 15:29:29 +0100

    fix version tsconfig-path to 4.2.0

--- src/common/config/app/configuration.ts
+++ src/common/config/app/configuration.ts
@@ -1,6 +1,4 @@
-// app/configuration.ts
 import { registerAs } from '@nestjs/config'
-
 export default registerAs('app', () => ({
   env: process.env.APP_ENV ?? 'dev',
   name: process.env.APP_NAME ?? 'LXP',
