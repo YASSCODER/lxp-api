81bec2a23a2511773ec38abd3e83c297e2a7d1f4
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2025-09-16 23:55:40 +0100

    create new endpoint to mark course as completed

--- src/modules/course/api/course.service.ts
+++ src/modules/course/api/course.service.ts
@@ -3,16 +3,22 @@ import { ErrorCodes } from '@/common/enum/error-codes.enum'
 import { Course } from '@/common/models/entities/course.entity'
 import { throwFormValidationError } from '@/common/utils/errors.utils'
 import { getCreateSuccessMessage } from '@/common/utils/success-messages.utils'
-import { Injectable } from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { CreateCourseDto } from '../dto/craete-course.dto'
+import { User } from '@/common/models/entities/user.entity'
+import { LearnerCourseLinker } from '@/common/models/entities/learner-course-linker.entity'
 
 @Injectable()
 export class CourseService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+    @InjectRepository(LearnerCourseLinker)
+    private readonly learnerCourseLinkRepository: Repository<LearnerCourseLinker>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async createCourse(payload: CreateCourseDto) {
@@ -42,5 +48,60 @@ export class CourseService {
       entityName: 'Course',
       entityId: courseSaved.id,
     })
+  }
+
+  async markAsCompleted(courseId: number, user: User) {
+    const userFound = await this.userRepository.findOne({
+      where: { id: user.id },
+    })
+
+    if (!userFound) {
+      throwFormValidationError({
+        errorCode: ErrorCodes.ENTITY_NOT_FOUND,
+        id: {
+          message: {
+            en: `user with id : ${user.id} not found`,
+            ar: `المستخدم بالمعرّف: ${user.id} غير موجود`,
+          },
+        },
+      })
+    }
+
+    const courseFound = await this.courseRepository.findOne({
+      where: {
+        id: courseId,
+      },
+    })
+
+    if (!courseFound) {
+      throwFormValidationError({
+        errorCode: ErrorCodes.ENTITY_NOT_FOUND,
+        id: {
+          message: {
+            en: `course with id : ${courseId} not found`,
+            ar: `الدورة بالمعرّف: ${courseId} غير موجودة`,
+          },
+        },
+      })
+    }
+
+    const learnerCourseLinkFound =
+      await this.learnerCourseLinkRepository.findOne({
+        where: {
+          courseId,
+        },
+      })
+
+    await this.learnerCourseLinkRepository.update(learnerCourseLinkFound.id, {
+      completed: true,
+    })
+
+    return {
+      status: HttpStatus.OK,
+      message: {
+        en: 'course completed',
+        ar: 'تم إكمال الدورة بنجاح',
+      },
+    }
   }
 }
--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { CourseService } from './api/course.service'
 import { PaginationService } from '@/common/pagination/pagination.service'
 import { CourseController } from './api/course.controller'
+import { User } from '@/common/models/entities/user.entity'
+import { LearnerCourseLinker } from '@/common/models/entities/learner-course-linker.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, User, LearnerCourseLinker])],
   providers: [CourseService, PaginationService],
   controllers: [CourseController],
   exports: [],
